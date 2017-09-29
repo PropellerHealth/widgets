4b4625ee59c1472edb7abea333abc503f7242dfc
mbrakken <marc.brakken@gmail.com>
2017-09-29 09:59:01 -0500

    Add in base widget with rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let props = document.getElementById('app-props').textContent;
+props = props.replace("<![CDATA[", "").replace("]]>", "");
+const data = JSON.parse(props);
+console.log(data);
+console.log(props);
+
+ReactDOM.render(<App {...data}/>, document.getElementById('ph-status'));
