90fd768f1a028d094eb36a077ceb4cc594fb6f7b
mbrakken <marc.brakken@gmail.com>
2017-10-02 14:23:50 -0500

    convert signup component to stateful and stub out methods

--- src/embeds/AsthmaConditions/Signup.js
+++ src/embeds/AsthmaConditions/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PoweredByPropeller from "./PoweredByPropeller";
 
 const PROPELLER_SIGNUP = "https://forecast-subscription.appspot.com/subscribe";
@@ -12,7 +12,7 @@ const styles = {
     fontSize: "0.9rem"
   },
 
-  orDivider : {
+  orDivider: {
     textAlign: "center"
   }
 };
@@ -44,58 +44,94 @@ const buttonStyle = {
   borderRadius: "6px"
 };
 
-const Signup = ({ style, flipCard }) => {
-  return (
-    <figure style={Object.assign({}, style, { transform: "rotateY(180deg)", zIndex: -1})}>
-      <h2 style={{margin: "0 0 0.5rem"}}>Signup for notifications</h2>
-      <form style={{textAlign: "left"}}>
-        <fieldset style={{border: "none", margin: 0, padding: 0}}>
-          <legend style={{fontSize: "0.9rem"}}>
-            Sign up to receive a quick text or email when your local asthma conditions are either fair or poor.
-          </legend>
-          <div className="clearfix">
-            <div>
-              <label for="email" style={styles.label}>Email</label>
-              <input
-                id="email"
-                style={inputStyle}
-                autocomplete="email"
-                type="email"
-              />
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: undefined,
+      sms: undefined,
+      postalCode: undefined
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e, key) {
+    this.setState({
+      [key]: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("signup", this.state);
+  }
+
+  render() {
+    const { style, flipCard } = this.props;
+    return (
+      <figure style={Object.assign({}, style, { transform: "rotateY(180deg)" })}>
+        <h2 style={{ margin: "0 0 0.5rem" }}>Signup for notifications</h2>
+        <form style={{ textAlign: "left" }} onSubmit={this.onSubmit}>
+          <fieldset style={{ border: "none", margin: 0, padding: 0 }}>
+            <legend style={{ fontSize: "0.9rem" }}>
+              Sign up to receive a quick text or email when your local asthma
+              conditions are either fair or poor.
+            </legend>
+            <div className="clearfix">
+              <div>
+                <label for="email" style={styles.label}>
+                  Email
+                </label>
+                <input
+                  id="email"
+                  style={inputStyle}
+                  autocomplete="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={(e) => this.onChange(e, "email")}
+                />
+              </div>
+              <div className="or-divider" style={styles.orDivider}>
+                or
+              </div>
+              <div>
+                <label for="sms" style={styles.label}>
+                  SMS Number
+                </label>
+                <input
+                  id="sms"
+                  style={inputStyle}
+                  autocomplete="tel-national"
+                  pattern="\d{10}"
+                  type="tel"
+                  value={this.state.sms}
+                  onChange={(e) => this.onChange(e, "sms")}
+                />
+              </div>
             </div>
-            <div className="or-divider" style={styles.orDivider}>
-              or
-            </div>
-            <div>
-              <label for="sms" style={styles.label}>SMS Number</label>
+            <div style={{ width: "50%", float: "left" }}>
+              <label for="postalCode" style={styles.label}>
+                Zip Code
+              </label>
               <input
-                id="sms"
+                id="postalCode"
                 style={inputStyle}
-                autocomplete="tel-national"
-                pattern="\d{10}"
+                required
                 type="tel"
+                pattern="\d{5}"
+                value={this.state.postalCode}
+                onChange={(e) => this.onChange(e, "postalCode")}
               />
             </div>
-          </div>
-          <div style={{width: "50%", float: "left"}}>
-            <label for="postalCode" style={styles.label}>Zip Code </label>
-            <input
-              id="postalCode"
-              style={inputStyle}
-              required
-              type="tel"
-            />
-          </div>
-          <input
-            style={buttonStyle}
-            type="submit"
-          />
-        </fieldset>
-      </form>
-      <button onClick={flipCard}>Back to forecast</button>
-      {/* <PoweredByPropeller /> */}
-    </figure>
-  );
-};
+            <input style={buttonStyle} type="submit" value="Sign up"/>
+          </fieldset>
+        </form>
+        <button onClick={flipCard}>Back to forecast</button>
+        {/* <PoweredByPropeller /> */}
+      </figure>
+    );
+  }
+}
 
 export default Signup;
