ee399a01c2e53b33ce5ca98dc0334990bbc02865
mbrakken <marc.brakken@gmail.com>
2017-10-02 14:21:02 -0500

    update styling method

--- src/embeds/AsthmaConditions/AsthmaScoreBar.js
+++ src/embeds/AsthmaConditions/AsthmaScoreBar.js
@@ -6,16 +6,56 @@ const STATUS_MAP = {
   high: "poor"
 };
 
-const AsthmaScoreBar = ({ status }) => {
+const THRESHOLDS = {
+  fair: 0.4,
+  poor: 0.55
+};
+
+const HEIGHT = "40px";
+
+const styles = {
+  wrapper: {
+    width: "100%",
+    height: HEIGHT,
+    lineHeight: HEIGHT,
+    marginBottom: "1.5rem",
+    backfaceVisibility: "hidden"
+  }
+};
+
+const markerPosition = score => {
+  console.log(typeof score);
+  if ("number" !== typeof score) return;
+
+  let position;
+  if (score > THRESHOLDS.fair) {
+    position = 0.33333 / (score / THRESHOLDS.fair);
+  } else if (score > THRESHOLDS.poor) {
+    position = 0.66667 + (0.3333 / ( 1 - score));
+  }
+
+  console.log(position);
+  return position;
+};
+
+const isActive = bool => bool ? "active" : "";
+
+const AsthmaScoreBar = ({ status, score }) => {
+  // console.log(status, score);
+  // console.log(typeof score);
+  const _status = STATUS_MAP[status];
   return (
-    <div className={`status-bar status-${STATUS_MAP[status]}`}>
-      <div className="status-section good">
+    <div
+      className="status-bar"
+      style={styles.wrapper}
+    >
+      <div className={`status-section good ${isActive("good" === _status)}`}>
         <span>Good</span>
       </div>
-      <div className="status-section fair">
+      <div className={`status-section fair ${isActive("fair" === _status)}`}>
         <span>Fair</span>
       </div>
-      <div className="status-section poor">
+      <div className={`status-section poor ${isActive("poor" === _status)}`}>
         <span>Poor</span>
       </div>
     </div>
