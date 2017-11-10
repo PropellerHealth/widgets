8c66e5433067a71202544fdcc987fb6737aa7b43
djwirz <djwirz@gmail.com>
2017-11-10 10:07:56 -0600

    Factored state names out of component and into utilities

--- src/embeds/FindMyDoctor/SearchScreen.js
+++ src/embeds/FindMyDoctor/SearchScreen.js
@@ -3,12 +3,13 @@ import Alert                      from "react-s-alert";
 import { FormGroup, FormControl } from "react-bootstrap";
 
 // import { checkResponse, extractJSON } from "../../utilities";
-import { checkResponse, extractJSON } from "../../utilities";
+import { checkResponse, extractJSON, stateInputList } from "../../utilities";
 
 const INPUT_FIELD = {
-  doctor    :  "Doctor's Name",
-  specialty :  "Specialty (optional)",
-  location  :  "State"
+  doctor    : "Doctor's Name",
+  specialty : "Specialty (optional)",
+  location  : "State",
+  city      : "City"
 };
 
 const ERRORS = {
@@ -45,13 +46,23 @@ class SearchScreen extends Component {
     this.state = {
       searching : false,
       doctor    : "", 
-      location  : "", 
-      specialty : "" 
+      location  : props.location, 
+      specialty : "",
+      city      : props.city 
     };
+
+    
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      location  : nextProps.location, 
+      city      : nextProps.city 
+    });  
+  }
+  
   buildRequest(doctor, specialty, location) {
     let allOfTheNames       =   `name=${doctor.trim().split(" ").join("%20")}`;
     let allOfTheLocations   =   `&location=${location.trim().split(" ").join("%20")}`;
@@ -76,7 +87,8 @@ class SearchScreen extends Component {
     const data = {
       doctor    : this.state.doctor,
       specialty : this.state.specialty,
-      location  : this.state.location
+      location  : this.state.location,
+      city      : this.state.city
     };
     
     if (!data.doctor && !data.location) {
@@ -143,6 +155,19 @@ class SearchScreen extends Component {
             <label
               style={{fontSize:"1.25rem"}}
             >
+              {INPUT_FIELD.city}:
+            </label>
+            <FormControl
+              id="city"
+              value={this.state.city}
+              onChange={(e) => this.onChange(e, "city")}
+              title={ERRORS.specialty}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label
+              style={{fontSize:"1.25rem"}}
+            >
               {INPUT_FIELD.location}:
             </label>
             <select
@@ -153,76 +178,24 @@ class SearchScreen extends Component {
               onChange={(e) => this.onChange(e, "location")}
               title={ERRORS.location}
             >
-              <option value=""></option>
-              <option value="AK">Alaska</option>
-              <option value="AL">Alabama</option>
-              <option value="AR">Arkansas</option>
-              <option value="AZ">Arizona</option>
-              <option value="CA">California</option>
-              <option value="CO">Colorado</option>
-              <option value="CT">Connecticut</option>
-              <option value="DC">District of Columbia</option>
-              <option value="DE">Delaware</option>
-              <option value="FL">Florida</option>
-              <option value="GA">Georgia</option>
-              <option value="HI">Hawaii</option>
-              <option value="IA">Iowa</option>
-              <option value="ID">Idaho</option>
-              <option value="IL">Illinois</option>
-              <option value="IN">Indiana</option>
-              <option value="KS">Kansas</option>
-              <option value="KY">Kentucky</option>
-              <option value="LA">Louisiana</option>
-              <option value="MA">Massachusetts</option>
-              <option value="MD">Maryland</option>
-              <option value="ME">Maine</option>
-              <option value="MI">Michigan</option>
-              <option value="MN">Minnesota</option>
-              <option value="MO">Missouri</option>
-              <option value="MS">Mississippi</option>
-              <option value="MT">Montana</option>
-              <option value="NC">North Carolina</option>
-              <option value="ND">North Dakota</option>
-              <option value="NE">Nebraska</option>
-              <option value="NH">New Hampshire</option>
-              <option value="NJ">New Jersey</option>
-              <option value="NM">New Mexico</option>
-              <option value="NV">Nevada</option>
-              <option value="NY">New York</option>
-              <option value="OH">Ohio</option>
-              <option value="OK">Oklahoma</option>
-              <option value="OR">Oregon</option>
-              <option value="PA">Pennsylvania</option>
-              <option value="PR">Puerto Rico</option>
-              <option value="RI">Rhode Island</option>
-              <option value="SC">South Carolina</option>
-              <option value="SD">South Dakota</option>
-              <option value="TN">Tennessee</option>
-              <option value="TX">Texas</option>
-              <option value="UT">Utah</option>
-              <option value="VA">Virginia</option>
-              <option value="VT">Vermont</option>
-              <option value="WA">Washington</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WV">West Virginia</option>
-              <option value="WY">Wyoming</option>
+              {stateInputList.map(state => <option key={state.name} value={state.abbreviation}>{state.name}</option>)}
             </select>
-          </FormGroup>
-          <br/>
-          <input
+            </FormGroup>
+            <br/>
+            <input
             style={
               this.state.searching
-                ? Object.assign({}, styles.formButton, {opacity: "0.5"})
-                : styles.formButton
+              ? Object.assign({}, styles.formButton, {opacity: "0.5"})
+              : styles.formButton
             }
             type="submit"
             value={this.state.searching ? "Searching..." : "Search"}
             disabled={this.state.searching}
-          />
-        </fieldset>
-      </form>
-    );
-  }
-}
-
-export default SearchScreen;
+            />
+            </fieldset>
+            </form>
+          );
+        }
+      }
+      
+      export default SearchScreen;
