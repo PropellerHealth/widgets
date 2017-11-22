df600403d7a2f0b947d17f5530153c075b4047ec
djwirz <djwirz@gmail.com>
2017-11-22 09:56:21 -0600

    Added logging for debugging the server

--- server/universal.js
+++ server/universal.js
@@ -14,12 +14,15 @@ const FORECAST_URL = "https://open.propellerhealth.com/prod/forecast";
 // consider moving this into utilities, only use geoip if going down certain rabbit holes
 const returnLocationFromIp = ip => {
   if ( ip ) {
+    console.log("the ip from the request used for location: ", ip);
     const geo = geoip.lookup(req.ip);
+    console.log("here is the geo object: ", geo);
     if ( geo ) {
       const latitude          = geo.ll[0];
       const longitude         = geo.ll[1];
       const forecastLocation  = `${geo.city}, ${geo.region}, ${geo.country}`;
       
+      console.log("test of what is beign returned: ", JSON.stringify({latitude, longitude, forecastLocation}));
       return { latitude, longitude, forecastLocation };
     } else {
       return undefined;
@@ -27,47 +30,41 @@ const returnLocationFromIp = ip => {
   } else {
     return undefined;
   }
-}
+};
 
 const propsForRequest = (req, cb) => {
-  if (req.path === "/asthma-conditions") {
-    // const geo = geoip.lookup(req.ip);
-    // if (geo) {
-    //   const lat = geo.ll[0];
-    //   const lng = geo.ll[1];
-      
-    //   let props = {
-    //     latitude         : lat,
-    //     longitude        : lng,
-    //     forecastLocation : `${geo.city}, ${geo.region}, ${geo.country}`
-    //   };
+  console.log("checking the request path: ", req.path);
+  if ( req.path === "/asthma-conditions") {
     const props = returnLocationFromIp(req.ip);
-
+    console.log("Props inside ac: ", props);
     if ( props ) {
-      request.get(`${FORECAST_URL}?latitude=${latitude}&longitude=${longitude}`, (err, resp, body) => {
+      console.log("if indeed there were props");
+      request.get(`${FORECAST_URL}?latitude=${props.latitude}&longitude=${props.longitude}`, (err, resp, body) => {
         if (err) return cb(undefined, props);
         const data = JSON.parse(body);
         
         return cb(undefined, Object.assign({}, props, {
-          score: data.properties.value,
-          status: data.properties.code.toLowerCase()
+          score   : data.properties.value,
+          status  : data.properties.code.toLowerCase()
         }));
       });
     } else {
       return cb(undefined, {});
     }
-  } else if (req.path === "/find-my-doctor"){
+  } else if (req.path.substring(0,15) === "/find-my-doctor"){
+    console.log("the substring worked: ", req.path)
     const props = returnLocationFromIp(req.ip);
 
     if ( props ) {
-      return cb(undefined, props)
+      return cb(undefined, props);
     } else {
       return cb(undefined, {});
     }
   } else {
+    console.log("calling back undefined for the path");
     return cb(undefined, {});
   }
-}
+};
 
 module.exports = function universalLoader(req, res) {
   const filePath = path.resolve(__dirname, '..', 'build', 'index.html');
