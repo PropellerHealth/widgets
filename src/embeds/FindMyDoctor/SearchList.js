18a7b547e4bed83c94d2d328b792cfdbf1d164b4
djwirz <djwirz@gmail.com>
2017-11-26 17:33:14 -0600

    Added screen for no results

--- src/embeds/FindMyDoctor/SearchList.js
+++ src/embeds/FindMyDoctor/SearchList.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 import { Grid, Button }     from "react-bootstrap";
 
+const styles = {
+  button : {
+    display         : "inline-block",
+    float           : "center",
+    width           : "50%",
+    marginTop       : "15px",
+    padding         : "12px 0",
+    fontSize        : "1.75rem",
+    textAlign       : "center",
+    backgroundColor : "gray",
+    color           : "#FFF",
+    cursor          : "pointer"
+  }
+};
+
 class SearchList extends Component {
   constructor(props) {
     super(props);
@@ -26,77 +41,149 @@ class SearchList extends Component {
     const doctors = this.state.doctors;
 
     return (
-      <form onSubmit={this.onSubmit}>
-        <Grid>
-          <div className="row">
-            <div className="col-xs-6 col-md-8">
-              <h2>Select a doctor</h2>
-            </div>
-            <div 
-              className="col-xs-6 col-md-4"
-              style={{textAlign: "center"}}
+      <div>
+        {doctors.length === 0 &&
+        <div>
+          <div
+            className="row"
+          >
+            <div
+              className="col-md-8 col-md-6 col-md-offset-2"
+              style={{
+                position  : "static",
+                textAlign : "center"
+              }}
             >
+              <h1
+                style={{
+                  color : "gray"
+                }}
+              >Sorry we were unable to find your doctor</h1>
+            </div>
+            <hr/>
+          </div>
+          <div
+            className="row"
+          >
+            <div
+              className="col-md-8 col-md-6 col-md-offset-2"
+              style={{
+                position  : "static",
+                textAlign : "center",
+                marginTop : "15px"
+              }}
+            >
+              <h2>Search Tips</h2>
             </div>
           </div>
-          {
-            doctors.map(doctor => {
-              return(
-                <div key={doctor.uid}>
-                  <div
-                    style={{position: "relative"}} 
-                    className="row">
-                    <div className="col-xs-6 col-md-8">
-                      <h3
-                        style={{fontSize:"1.5rem"}}
-                      >{doctor.profile.first_name} {doctor.profile.last_name}<br/>
-                        </h3>
-                      {doctor.practices[0]    &&  <p>{doctor.practices[0].visit_address.street}, {doctor.practices[0].visit_address.street2} <br/>
-                        {doctor.practices[0].visit_address.city}, {doctor.practices[0].visit_address.state} {doctor.practices[0].visit_address.zip}</p>}
-                    </div>
-                    <div 
-                      className="col-xs-6 col-md-4"
-                      style={{
-                        position  : "static",
-                        textAlign : "center"
-                      }}
-                    >
-                      <Button
-                        style={{
-                          position  : "absolute",
-                          top       : "50%",
-                          marginTop : "-17px"
-                        }}
-                        bsStyle="success"
-                        onClick={() => this.handleClick(doctor)}>
-                        Select
-                      </Button>
-                    </div>
-                  </div>
-                  <hr/>
-                </div>
-              );
-            })
-          }
-          <div className="col-md-6 col-md-offset-3">
-            <Button
+          <div
+            className="row"
+          >
+            <div
+              className="col-md-8 col-md-6 col-md-offset-2"
               style={{
-                display         : "inline-block",
-                float           : "center",
-                width           : "50%",
-                marginTop       : "15px",
-                padding         : "12px 0",
-                fontSize        : "1.75rem",
-                textAlign       : "center",
-                backgroundColor : "gray",
-                color           : "#FFF",
-                cursor          : "pointer"
+                position  : "static",
+                textAlign : "center"
               }}
+            >
+              <ul 
+                className="text-left" 
+                style={{
+                  display       : "inline-block",
+                  verticalAlign : "middle"
+                }}
+              >
+                <li>Check your spelling and try again</li>
+                <li>Try another specialty</li>
+                <li>Search by last name only</li>
+                <li>Verify your doctor's primary location</li>
+              </ul>
+            </div>
+          </div>
+          <div
+            className="row"
+            style={{
+              textAlign : "center",
+              marginTop : "45px"
+            }}
+          >
+            <Button
+              style={styles.button}
               onClick={() => this.handleClick()}>
               Back to Search
             </Button>
           </div>
-        </Grid>
-      </form>
+        </div>
+        }
+        {doctors.length > 0 &&
+        <form onSubmit={this.onSubmit}>
+          <Grid>
+            <div className="row">
+              <div className="col-xs-6 col-md-8">
+                <h2>Select a doctor</h2>
+              </div>
+              <div 
+                className="col-xs-6 col-md-4"
+                style={{textAlign: "center"}}
+              >
+              </div>
+            </div>
+            {
+              doctors.map(doctor => {
+                return(
+                  <div key={doctor.uid}>
+                    <div
+                      style={{position: "relative"}} 
+                      className="row">
+                      <div className="col-xs-6 col-md-8">
+                        <h3
+                          style={{fontSize:"1.5rem"}}
+                        >{doctor.profile.first_name} {doctor.profile.last_name}<br/>
+                          </h3>
+                        {doctor.practices[0]    &&  <p>{doctor.practices[0].visit_address.street}, {doctor.practices[0].visit_address.street2} <br/>
+                          {doctor.practices[0].visit_address.city}, {doctor.practices[0].visit_address.state} {doctor.practices[0].visit_address.zip}</p>}
+                      </div>
+                      <div 
+                        className="col-xs-6 col-md-4"
+                        style={{
+                          position  : "static",
+                          textAlign : "center"
+                        }}
+                      >
+                        <Button
+                          style={{
+                            position  : "absolute",
+                            top       : "50%",
+                            marginTop : "-17px"
+                          }}
+                          bsStyle="success"
+                          onClick={() => this.handleClick(doctor)}>
+                          Select
+                        </Button>
+                      </div>
+                    </div>
+                    <hr/>
+                  </div>
+                );
+              })
+            }
+          </Grid>
+          <div
+            className="row"
+            style={{
+              textAlign : "center",
+              marginTop : "45px"
+            }}
+          >
+            <Button
+              style={styles.button}
+              onClick={() => this.handleClick()}>
+              Back to Search
+            </Button>
+          </div>
+        </form>
+        }
+      </div>
     );
   }
 }
