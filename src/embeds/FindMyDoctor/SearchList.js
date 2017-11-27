b4b77db62a4644d112d3fdcbdb7ffc46ceff74cc
djwirz <djwirz@gmail.com>
2017-11-27 09:56:05 -0600

    Changed specialty viewing property to match search list

--- src/embeds/FindMyDoctor/SearchList.js
+++ src/embeds/FindMyDoctor/SearchList.js
@@ -139,7 +139,7 @@ class SearchList extends Component {
                           style={{fontSize:"1.5rem"}}
                         >{doctor.profile.first_name} {doctor.profile.last_name}<br/>
                         </h3>
-                        {(doctor.specialties[0]) && <p>{doctor.specialties[0].actor}</p>}
+                        {(doctor.specialties[0]) && <p>{doctor.specialties[0].name}</p>}
                         {doctor.practices[0]    &&  <p>{doctor.practices[0].visit_address.street}, {doctor.practices[0].visit_address.street2} <br/>
                           {doctor.practices[0].visit_address.city}, {doctor.practices[0].visit_address.state} {doctor.practices[0].visit_address.zip}</p>}
                       </div>
