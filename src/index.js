fb75e2cc1ea002812df6a3379c8a14c4c9e0a1fd
mbrakken <marc.brakken@gmail.com>
2018-04-20 13:06:30 -0500

    [ATLAS-1794] disable test data

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import i18n from "./i18n";
 import App from "./App";
 import "./index.css";
 
-import testData from "./TEST_DATA.json";
+// import testData from "./TEST_DATA.json";
 
 let el = document.getElementById("app-props");
 let props;
@@ -14,8 +14,8 @@ let data;
 if (el) {
   props = el.textContent.replace("<![CDATA[", "").replace("]]>", "");
   data = JSON.parse(props);
-} else if (testData) {
-  data = testData;
+// } else if (testData) {
+  // data = testData;
 }
 
 const renderMethod = el
