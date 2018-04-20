31d09662341a6c0d20584f93d4cb92b9734f9dc0
mbrakken <marc.brakken@gmail.com>
2018-04-20 12:19:28 -0500

    [ATLAS-1794] convert dates to moment objects, set to patient timezone

--- src/embeds/PatientReport/index.js
+++ src/embeds/PatientReport/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Loading from "../../components/Loading";
 import PatientSummary from "./PatientSummary";
-import moment from "moment";
+import moment from "moment-timezone";
 
 import {
   checkResponse,
@@ -12,13 +12,11 @@ import {
 
 import "./index.css";
 
-const incrementToDays = (toCopy, days) => {
+const incrementToDays = timeZone => (toCopy, days) => {
   const ary = new Array(days);
   for (let i = 0; i < days; i++) {
     ary[i] = Object.assign({}, toCopy, {
-      date: moment(toCopy.date)
-        .add(i, "days")
-        .toDate()
+      date: moment(toCopy.date).tz(timeZone).add(i, "days")
     });
   }
   return ary;
@@ -76,7 +74,13 @@ class PatientReport extends Component {
     if (!patient) return <Loading />;
 
     // do all of our heavy data munging here and just feed the result downstream
-    const { controlStatus = [], days = [], trends = {} } = dailySummary;
+    const { controlStatus = [], days : _days = [], trends = {} } = dailySummary;
+    const { timeZone } = patient;
+
+    const days = _days.map(d => {
+      d.date = moment(d.date).tz(timeZone);
+      return d;
+    });
 
     const rescueNights = days.filter(d => d.rescue.nightEvents > 0).length;
     const rescueMeds   = medications.filter(m => "rescue" === m.medication.type);
@@ -86,10 +90,10 @@ class PatientReport extends Component {
       sortedDays[sortedDays.length - 1]
     ].map(m => new Date(m.date));
 
-    const oldestLastSync = medications
+    const oldestLastSync = moment(medications
       .map(med => med.sensors.map(s => s.lastSyncDate))
       .reduce((ary, times) => ary.concat(times))
-      .sort(sortDates)[0];
+      .sort(sortDates)[0]);
 
     const adherence  = controllerAdherence.sort(sortDates);
 
@@ -98,13 +102,14 @@ class PatientReport extends Component {
       .map(med => {
         med.adherenceByWeek = adherence
           .map(week => ({
-            date   : new Date(week.date),
+            date   : moment(week.date).tz(timeZone),
             values : week.medications && week.medications.find(m => med.medicationId === m.mid)
           }))
-          .reduce((arr, d) => arr.concat(incrementToDays(d, 8)), []);
+          .sort(sortDates)
+          .reduce((arr, d) => arr.concat(incrementToDays(timeZone)(d, 8)), []);
 
         med.adherenceByDay = sortedDays.map(day => ({
-          date   : new Date(day.date),
+          date   : moment(day.date).tz(timeZone),
           values : day.controller && day.controller.meds.find(m => m.mid === med.medicationId)
         }));
         return med;
